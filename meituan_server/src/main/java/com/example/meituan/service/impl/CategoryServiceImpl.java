33c521bfc4820a1e36e30299703ed46b49dcd345
WangMinan <wangminan0811@hotmail.com>
2023-02-16 13:38:59 +0800

    修正了系列问题

--- meituan_server/src/main/java/com/example/meituan/service/impl/CategoryServiceImpl.java
+++ meituan_server/src/main/java/com/example/meituan/service/impl/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.meituan.constants.CategoryConstants;
 import com.example.meituan.exception.SearchException;
 import com.example.meituan.pojo.R;
 import com.example.meituan.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -26,62 +27,46 @@ import java.util.Map;
 
 import static com.example.meituan.constants.FoodIndexConstants.MEITUAN_INDEX_NAME;
 
+/**
+ * @author 林昊辰 wangminan
+ */
 @Service
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
 
-    private static final Map<String, Integer> categoryMap = CategoryConstants.initCategoryMap();
+    private static Map<String, Integer> categoryMap = CategoryConstants.initCategoryMap();
 
     private static final String CATEGORY_AGG_NAME = "CategoryAggression";
     private static final String CATEGORY_FIELD_NAME = "category";
     private static final String RESULT = "result";
+
 
     @Resource
     private RestHighLevelClient restHighLevelClient;
 
     @Override
     public R getTotalShopByCategory(){
-        SearchResponse response =
-                getSearchResponse(QueryBuilders.matchAllQuery());
-        // 解析聚合结果
-        Aggregations aggregations = response.getAggregations();
-        getNumberOfMerchantsByAgg(aggregations);
+        putMerchantNumberToMap(QueryBuilders.matchAllQuery());
         return R.ok().put(RESULT, categoryMap);
     }
 
     @Override
     public R getAvgPriceByCategory(){
+        // 重置 categoryMap
+        categoryMap = CategoryConstants.initCategoryMap();
         Map<String, Integer> avgPriceMap = new HashMap<>();
         RangeQueryBuilder queryBuilder =
                 QueryBuilders
                         .rangeQuery("avgPrice")
                         .gt(0);
-        SearchResponse response =
-                getSearchResponse(queryBuilder);
-        // 解析聚合结果
-        Aggregations aggregations = response.getAggregations();
-        getNumberOfMerchantsByAgg(aggregations);
+        putMerchantNumberToMap(queryBuilder);
 
         for (Map.Entry<String, Integer> cate : categoryMap.entrySet()) {
             String category = cate.getKey();
-            BoolQueryBuilder queryBuilder1 =
-                    QueryBuilders.boolQuery()
-                            .must(QueryBuilders.wildcardQuery(CATEGORY_FIELD_NAME, "*" + category + "*"));
-            SearchRequest searchRequest = new SearchRequest(MEITUAN_INDEX_NAME);
-            searchRequest.source()
-                    .query(queryBuilder1)
-                    .aggregation(
-                            AggregationBuilders
-                                    .sum("totalPrice")
-                                    .field("avgPrice")
-                    );
-            SearchResponse searchResponse;
-            try {
-                searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-            } catch (IOException e) {
-                throw new SearchException("查询价格失败");
-            }
-            Aggregations aggregations1 = searchResponse.getAggregations();
-            Sum totalPrice = aggregations1.get("totalPrice");
+            SearchResponse searchResponse =
+                    getSumAggSearchResponse(category, "totalPrice", "avgPrice");
+            Aggregations aggregations = searchResponse.getAggregations();
+            Sum totalPrice = aggregations.get("totalPrice");
             // 转换 double转int
             avgPriceMap.put(category,
                     totalPrice.getValue() == 0 ? 0: (int) totalPrice.getValue() / cate.getValue());
@@ -91,39 +76,69 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public R getTotalCommentByCategory(){
+        // 重置 categoryMap
+        categoryMap = CategoryConstants.initCategoryMap();
         Map<String, Integer> totalCommentMap = new HashMap<>();
-        SearchResponse response =
-                getSearchResponse(QueryBuilders.matchAllQuery());
-        // 解析聚合结果
-        Aggregations aggregations = response.getAggregations();
-        getNumberOfMerchantsByAgg(aggregations);
+        putMerchantNumberToMap(QueryBuilders.matchAllQuery());
 
         for (Map.Entry<String, Integer> cate : categoryMap.entrySet()) {
             String category = cate.getKey();
-            BoolQueryBuilder queryBuilder1 =
-                    QueryBuilders.boolQuery()
-                            .must(QueryBuilders.wildcardQuery(CATEGORY_FIELD_NAME, "*" + category + "*"));
-            SearchRequest searchRequest = new SearchRequest(MEITUAN_INDEX_NAME);
-            searchRequest.source()
-                    .query(queryBuilder1)
-                    .aggregation(
-                            AggregationBuilders
-                                    .sum("totalComment")
-                                    .field("allCommentNum")
-                    );
-            SearchResponse searchResponse;
-            try {
-                searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-            } catch (IOException e) {
-                throw new SearchException("查询评论数失败");
-            }
-            Aggregations aggregations1 = searchResponse.getAggregations();
-            Sum totalComment = aggregations1.get("totalComment");
+            SearchResponse searchResponse =
+                    getSumAggSearchResponse(category, "totalComment", "allCommentNum");
+            Aggregations aggregations = searchResponse.getAggregations();
+            Sum totalComment = aggregations.get("totalComment");
             // 转换 double转int
-            totalCommentMap.put(category,
-                    totalComment.getValue() == 0 ? 0: (int) totalComment.getValue());
+            totalCommentMap.put(category, (int) totalComment.getValue());
         }
+        // 重置 categoryMap
+        categoryMap = CategoryConstants.initCategoryMap();
         return R.ok().put(RESULT, totalCommentMap);
+    }
+
+    private void putMerchantNumberToMap(QueryBuilder queryBuilder){
+        SearchResponse response =
+                getAggSearchResponse(queryBuilder);
+        // 解析聚合结果
+        Aggregations aggregations = response.getAggregations();
+        getNumberOfMerchantsByAgg(aggregations);
+    }
+
+    private SearchResponse getAggSearchResponse(QueryBuilder queryBuilder) {
+        SearchRequest searchRequest = new SearchRequest(MEITUAN_INDEX_NAME);
+        searchRequest.source()
+                .query(queryBuilder)
+                .aggregation(
+                        AggregationBuilders
+                                .terms(CategoryServiceImpl.CATEGORY_AGG_NAME)
+                                .field(CategoryServiceImpl.CATEGORY_FIELD_NAME)
+                                .size(600)
+                );
+        // 发送请求
+        try {
+            return restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            throw new SearchException("查询失败");
+        }
+    }
+
+    private SearchResponse getSumAggSearchResponse(String category, String sumName, String sumField) {
+        BoolQueryBuilder queryBuilder =
+                QueryBuilders.boolQuery()
+                        .must(QueryBuilders.wildcardQuery(CATEGORY_FIELD_NAME, "*" + category + "*"));
+        SearchRequest searchRequest = new SearchRequest(MEITUAN_INDEX_NAME);
+        searchRequest.source()
+                .query(queryBuilder)
+                .aggregation(
+                        AggregationBuilders
+                                .sum(sumName)
+                                .field(sumField)
+                );
+        // 发送请求
+        try {
+            return restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            throw new SearchException("求和失败");
+        }
     }
 
     private void getNumberOfMerchantsByAgg(Aggregations aggregations) {
@@ -140,24 +155,6 @@ public class CategoryServiceImpl implements CategoryService {
                     categoryMap.put(cate.getKey(), cate.getValue() + categoryDto.getValue());
                 }
             }
-        }
-    }
-
-    private SearchResponse getSearchResponse(QueryBuilder queryBuilder) {
-        SearchRequest searchRequest = new SearchRequest(MEITUAN_INDEX_NAME);
-        searchRequest.source()
-                .query(queryBuilder)
-                .aggregation(
-                        AggregationBuilders
-                                .terms(CategoryServiceImpl.CATEGORY_AGG_NAME)
-                                .field(CategoryServiceImpl.CATEGORY_FIELD_NAME)
-                                .size(600)
-                );
-        // 发送请求
-        try {
-            return restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
-        } catch (IOException e) {
-            throw new SearchException("查询评价失败");
         }
     }
 
