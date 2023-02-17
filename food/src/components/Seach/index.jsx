8cc34573f3190c6ad0099136c9733d3a2c0f5bff
13379299616 <1259767428@qq.com>
2023-02-17 15:26:10 +0800

    7

--- food/src/components/Seach/index.jsx
+++ food/src/components/Seach/index.jsx
@@ -2,16 +2,13 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import { Input } from 'antd';
 import { AutoComplete,Pagination,Button,Radio } from 'antd'
-import { DownOutlined } from '@ant-design/icons';
-// import { MenuProps } from 'antd';
-import { Dropdown, Space } from 'antd';
 
 // const { Search } = Input;
 const { TextArea } = Input;
 
 const clas=["全部（不启用筛选）","代金券","蛋糕甜点","火锅","自助餐","小吃快餐","日韩料理","西餐","聚餐宴请","烧烤烤肉","东北菜","川湘菜","江浙菜","香锅烤鱼","粤菜","中式烧烤_烤串","西北菜","咖啡酒吧","京菜鲁菜","云贵菜","东南亚菜","海鲜","素食","台湾_客家菜","创意菜","汤_粥_炖菜","蒙餐","新疆菜","其他美食"]
 export default class Seach extends Component {
-    state={textvalue:[],dataSource:[],pagesize:5,urls:[],pages:0,nowSeach:null,pagemod:0,needmod:false,classchack:null,reutrned:false,pageHeight:163.6,subtitles:null}
+    state={textvalue:[],dataSource:[],pagesize:5,urls:[],pages:0,nowSeach:null,pagemod:0,needmod:false,classchack:null,reutrned:false,pageHeight:163.6,subtitles:[[<option>--请先选择区划--</option>]],leftsub:[],rightuse:0,busD:null,bds:[]}
     componentDidMount(){
         axios.defaults.baseURL='https://meituan.wangminan.me'
         axios({
@@ -23,20 +20,30 @@ export default class Seach extends Component {
             // console.log(response.data.result)
             let subtitles=[]
             let tmp=Object.keys(result)
+            //双下拉菜单 左
+            let leftsub=[]
+            leftsub.push(<option>--请选择区划--</option>)
+            for(let i=0;i<tmp.length;i++){
+                leftsub.push(
+                    <option>{tmp[i]}</option>
+                )
+            }
+            this.setState({leftsub})
+
+            //双下拉菜单 右
             let tmpp=Object.values(result)
+            this.setState({bds:tmpp})
+            subtitles.push([<option>--请先选择区划--</option>])
             for(let i=0;i<tmp.length;i++){
                 let tpmenu=tmpp[i]
-                let menuitem={}
-                menuitem.key=i
-                menuitem.label=tmp[i]
-                menuitem.children=[]
+                let rightitem=[]
+                rightitem.push(<option>--请选择商圈--</option>)
                 for(let j=0;j<tpmenu.length;j++){
-                    let inneritem={key:i+'.'+j,label:tpmenu[j]}
-                    menuitem.children.push(inneritem)
+                    rightitem.push(<option>{tpmenu[j]}</option>)
                 }
-                subtitles.push(menuitem)
+                subtitles.push(rightitem)
             }
-            this.setState(subtitles)
+            this.setState({subtitles})
         })
     }
 
@@ -77,6 +84,7 @@ export default class Seach extends Component {
                 pageNum:1,
                 pageSize:this.state.pagesize,
                 type:this.state.classchack,
+                businessDistrict:this.state.busD,
             }
         }).then(response=>{
             const {pagesize}=this.state
@@ -122,6 +130,7 @@ export default class Seach extends Component {
                 pageNum:Num,
                 pageSize:this.state.pagesize,
                 type:this.state.classchack,
+                businessDistrict:this.state.busD,
             }
         }).then(response=>{
             const {merchants}=response.data.result
@@ -170,6 +179,23 @@ export default class Seach extends Component {
         }
     }
 
+    onMenuChange=(e)=>{
+        // console.log(e.target.selectedIndex)
+        this.setState({rightuse:e.target.selectedIndex})
+    }
+
+    onMenuChange1=(e)=>{
+        const {selectedIndex}=e.target
+        if(selectedIndex===0){
+            this.setState({busD:null})
+        }else{
+            const arr=this.state.bds[this.state.rightuse-1]
+            let bu=arr[selectedIndex-1]
+            this.setState({busD:bu})
+        }
+        console.log(this.state.busD)
+    }
+
   render() {
     const {dataSource}=this.state
     var items=[]
@@ -201,14 +227,12 @@ export default class Seach extends Component {
     if(this.state.reutrned){
         return (
         <div>
-            <Dropdown menu={ this.state.subtitles }>
-                <a onClick={(e) => e.preventDefault()}>
-                <Space>
-                    选择分区
-                    <DownOutlined />
-                </Space>
-                </a>
-            </Dropdown>
+            <select id='left' onChange={this.onMenuChange} >
+                {this.state.leftsub}
+            </select>
+            <select id='right' onChange={this.onMenuChange1} >
+                {this.state.subtitles[this.state.rightuse]}
+            </select>
             <Radio.Group defaultValue="0" buttonStyle="solid" onChange={this.onClassChange} >
                 {radioitem}
             </Radio.Group>
@@ -235,14 +259,12 @@ export default class Seach extends Component {
     }else{
         return(
         <div>
-            <Dropdown menu={ this.state.subtitles }>
-                <a onClick={(e) => e.preventDefault()}>
-                <Space>
-                    选择分区
-                    <DownOutlined />
-                </Space>
-                </a>
-            </Dropdown>
+            <select id='left' onChange={this.onMenuChange} >
+                {this.state.leftsub}
+            </select>
+            <select id='right' onChange={this.onMenuChange1} >
+                {this.state.subtitles[this.state.rightuse]}
+            </select>
             <Radio.Group defaultValue="" buttonStyle="solid" onChange={this.onClassChange} >
                 {radioitem}
             </Radio.Group>
